c323b1092a48f5e3f894b302a4533321ab7ff8e0
Ethem Fatih Hocaoğlu <66084850+kapo1103@users.noreply.github.com>
2023-01-04 18:24:53 +0300

    Odev1

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+var sehir : String = "İstanbul"
+var ulke : String = "Türkiye"
+var telefon : Int = 05319510957
+var postaKodu : Int = 34734
+var email : String = "ysfatih1@gmail.com"
+var meslek : String = "Ios Developer"
+var stokMiktari : Int = 500
+var musteriAdi : String = "Fatih"
+var bakiye : Float = 43.95
+let dogumGunu : String = "11 Mart 1998"
+let maas : Float = 8506.80
+var medeniDurum : String = "Bekar"
+var urunYorumu : String = "Fiyatına göre çok iyi"
+let odemeTarihi = "3 ocak"
+let odeme = 100
+var siparisAdedi = 13
+var arabaModeli = "Mercedes E200 "
+var kitapAdi = "Aşkname"
+let yayinlamaTarihi = "3 ocak 2023"
+var indirimMiktari = "%30"
+let odaSayisi = 3
+var enlem = "41°07′ 00″ K"
+var boylam = "29°54′00″ D"
+var urunAdi = "Macbook"
+var yemekFiyati = 50
+var marka = "Apple"
+var muzikAdi = "Lovely"
+var videoSuresi = 4.12
+var urunPuani = 4.8
+var resimAdi = "manzara"
+var dosyaFormati = "Pdf"
+var renk = "sarı"
+var renkKodu = "#ffff00"
+var telefonModeli = "Huawei P20 Lite"
+let ekranBoyutu = 27
+var agirlik = 70
+let ulusalGun = "23 Nisan"
+let tatilGunu = " 1 Ocak"
+let rezervasyonTarihi = "15 Mart 2023"
+let sokakAdi = "Orta"
+let otobusHatti = "19F"
+var kalanDakika = 15
+let takipKodu = "FAH658KSD92"
+var kuponSuresi = 234
+let kuponKodu = "WELCOME2023"
+let faturaAdresi = "KADİKÖY/İSTANBUL"
+
+print("Yaşadığım Şehir \(sehir)")
+print("Yaşadığım Ülke \(ulke)")
+print("Telefon Numaram \(telefon)")
+print("Posta Kodu:\(postaKodu)")
+print("Email Adresim: \(email)")
+print("Mesleğim \(meslek)")
+print("Stok Miktarı:\(stokMiktari) Adet")
+print("Müşteri Adı:\(musteriAdi)")
+print("Bakiye:\(bakiye)₺")
+print("Dogum Günüm \(dogumGunu)")
+print("Maaşım \(maas) ₺")
+print("Medeni Durumum \(medeniDurum)")
+print("Ürün Yorumları: \(urunYorumu)")
+print("Ödeme Tarihi:\(odemeTarihi)")
+print(odeme, "₺ ödedim")
+print(siparisAdedi, "tane sipariş verdim")
+print(arabaModeli, "bu arabayı almak istiyorum")
+print(kitapAdi, "en sevdiğim kitaptır")
+print(yayinlamaTarihi, "tarihinde ios bootcamp'i başladı")
+print(indirimMiktari,"indirim uygulanmıştır")
+print("Evimde \(odaSayisi) oda var")
+print("Kadiköyün Enlemi:\(enlem)")
+print("Kadiköyün Boylamı:\(boylam)")
+print(urunAdi, "aldım")
+print("Yemeğin fiyatı \(yemekFiyati)₺ ")
+print(marka, "ürünleri çok iyi")
+print("En sevdiğim müzik \(muzikAdi)")
+print("Videonun Süresi \(videoSuresi) saniye")
+print("Ürün puanı:\(urunPuani)")
+print(resimAdi, "resimlerini severim")
+print("Dosya formatı:\(dosyaFormati)")
+print("En sevdiğim renk \(renk)")
+print("Sarının renk kodu:\(renkKodu)")
+print("Kullandığım telefonun modeli \(telefonModeli)")
+print("Bilgisayarımın ekran boyutu \(ekranBoyutu) inç")
+print(agirlik, "kiloyum")
+print(ulusalGun, "Ulusal gündür")
+print(tatilGunu, "Tatildir")
+print("Rezervasyon tarihi:\(rezervasyonTarihi)")
+print(sokakAdi, "Sokak")
+print("En çok kullandığım otobüs hattı \(otobusHatti)")
+print("Maçın Başlamasına \(kalanDakika) dakika kaldı")
+print("Takip Kodu:\(takipKodu)")
+print("Kupon süresi:\(kuponSuresi) gün")
+print("Kupon Kodu:\(kuponKodu)")
+print("Fatura Adresi:\(faturaAdresi)")
+
