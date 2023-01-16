16bd5c55f7e0d0410ba4799495f9533708dc10b4
Ethem Fatih Hocaoğlu <66084850+kapo1103@users.noreply.github.com>
2023-01-17 01:48:18 +0300

    Odev4

---
+++ Odev4/SayfaY.swift
@@ -0,0 +1,22 @@
+//
+//  SayfaY.swift
+//  Odev4
+//
+//  Created by Ethem Fatih Hocaoğlu on 17.01.2023.
+//
+
+import UIKit
+
+class SayfaY: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+    @IBAction func buttonBasaDon(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+
+    }
+}
---
+++ Odev4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Odev4
+//
+//  Created by Ethem Fatih Hocaoğlu on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
